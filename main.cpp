e9d7f7592949f9a3904ac8f7693ebf39461a3762
Kou$htav Chakrabarty <lostsoulofthegeek@gmail.com>
2013-09-28 21:16:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,8 @@
-//==============//
-//   LISCENSE   //
-//==============//
-
-
-/*******************************************************************************************************************
- *                                                                                                                 *
- *  The zlib/libpng License (Zlib) [Open Source Initiative Approved License]                                       *
- *  Copyright (c) 2013, Koushtav Chakrabarty(The illusionist mirage)                                               *
- *                                                                                                                 *
- *                                                                                                                 *
- *  Before using this source code, you must read and agree to the following conditions:                            *           *
- *                                                                                                                 *
- *                                                                                                                 *
- *  This software is provided 'as-is', without any express or implied warranty. In no                              *
- *  event will I, the author, be held liable for any damages arising from the use of this software.                *
- *                                                                                                                 *
- *  Permission is granted to anyone to use this software for any purpose, including                                *
- *  commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:    *
- *                                                                                                                 *
- *     0. This program uses SFML v 2.1, which itself was licensed under zlib(https://github.com/LaurentGomila/     *
- *        SFML/blob/master/license.txt).                                                                           *
- *                                                                                                                 *
- *     1. The origin of this software must not be misrepresented; you must not claim that you wrote                *
- *        the original software. If you use this software in a product, an acknowledgment in the                   *
- *        product documentation would be appreciated but is not required.                                          *
- *                                                                                                                 *
- *     2. Altered source versions must be plainly marked as such, and must not be misrepresented as                *
- *        being the original software.                                                                             *
- *                                                                                                                 *
- *     3. This notice may not be removed or altered from any source distribution.                                  *
- *                                                                                                                 *
- *******************************************************************************************************************/
-
-
-
+/**********************************************************************
+*  Copyright (c) 2013, Koushtav Chakrabarty(The Illusionist Mirage)   *
+*  License: zlib/libpng License                                       *
+*  zlib/libpng License web page: http://opensource.org/licenses/Zlib  *
+**********************************************************************/
 
 /*************
 *  main.cpp  *
